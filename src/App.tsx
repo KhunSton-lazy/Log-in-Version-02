740c19d70c2726bcc08ea1cbe9416dc85d2aca3b
Khun ston <cocacolamonster15@gmail.com>
2021-12-23 21:13:42 +0700

    version 03

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import TextField      from '@mui/material/TextField';
 import Checkbox       from '@mui/material/Checkbox';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Logo1 from "./images/RRPS-logo_1.png";
 
-import "./styles/styles.css"
+import "./styles/styles.css";
 
 
 
@@ -14,7 +15,6 @@ function App() {
   const [phoneNumber, setPhoneNumber] = useState<number>()
   const [password, setPhonePassword] = useState<string>("")
   
-
   const handlePhoneNumber = (event: { target: { value: any; }; }) => {
     const updatePhoneNumber = event.target.value;
     setPhoneNumber(updatePhoneNumber)
@@ -26,66 +26,64 @@ function App() {
   }
   
   return (
-    <div className="container">   
+    <div className="container">  
+    
      <Box
       component="form"
       sx={{
-        '& .MuiTextField-root': { m: 1, width: '50ch'},
+        '& .MuiTextField-root': { m: 1, width: "60ch"},
       }}
       noValidate
       autoComplete="off" 
     > 
     <div className="content">
-      
+
       <div>
-        <Typography variant="h4">
+        <Typography className="text" variant="h4">
           <b>Log In</b>
         </Typography>
-        <Typography variant="body1">
+        <Typography className="text" variant="body1">
           Doesn't have an account yet? <span style={{color: "#59BBE5"}}>Sign up</span>
         </Typography>
       </div>
      
       <div className="phoneNumber">
-      <Typography variant="subtitle1">
+      <Typography className="text" variant="subtitle1">
         Phone number
       </Typography>
       <TextField
+          className="textField"
           required
           id="outlined-required"
           label="+855"
           value={phoneNumber}
           onChange={handlePhoneNumber}
         />
-        <Typography variant="subtitle1">
-          {phoneNumber}
-        </Typography>
       </div>
       
       <div className="password">
         <div className="forgotPassword">
-          <Typography variant="subtitle1">
+          <Typography className="text" variant="subtitle1">
           Password
           </Typography>
-          <Typography variant="subtitle1" style={{color: "#59BBE5"}}>
+          <Typography className="text" variant="subtitle1" style={{color: "#59BBE5"}}>
           Forgot Password?
           </Typography>
         </div>
 
       <TextField
           required
+          className="textField"
           id="outlined-required"
           type="password"
           value={password}
           onChange={handlePassword}
         />
-        <Typography>
-          {password}
-        </Typography>
+
         <br />
         <div className="rememberMe">
           <Checkbox />
-          <Typography variant="subtitle1">
+          <Typography className="text" variant="subtitle1">
           Remember me
         </Typography>
         </div>
@@ -94,7 +92,7 @@ function App() {
       </div>
       </div>
      </Box>
-     
+     <img src={Logo1} className="logo" alt="logo"/>
      </div>
   );
 }
